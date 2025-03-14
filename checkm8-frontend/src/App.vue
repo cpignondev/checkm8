a13e5e1a641a6bc4882750b5887fd78ae30829df
Camille Pignon <cpignon.dev@gmail.com>
2025-03-14 08:06:44 +0100

    Displayed a chess board on the page
    * Installed 'vue3-chessboard'
    * Reset the App.vue
    * Integrated 'vue3-chessboard' to display a board on the page

--- checkm8-frontend/src/App.vue
+++ checkm8-frontend/src/App.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+import type { BoardApi, BoardConfig } from 'vue3-chessboard';
+
+let boardAPI: BoardApi;
+
+const boardConfig: BoardConfig = reactive({
+  coordinates: true,
+  orientation: 'white',
+  position: 'start',
+});
+
+function onBoardCreated(api: BoardApi) {
+  boardAPI = api;
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h2>CheckM8</h2>
+
+  <!-- Plateau -->
+  <TheChessboard
+    :board-config="boardConfig"
+    reactive-config
+    @board-created="onBoardCreated">
+  </TheChessboard>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
